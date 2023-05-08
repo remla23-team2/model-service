fd8d2c3230a8487a9858538f84fd9b64c42f8105
SiradaKa <117922132+SiradaKa@users.noreply.github.com>
2023-05-08 16:28:26 +0200

    Added changes

--- Dockerfile
+++ Dockerfile
@@ -10,8 +10,16 @@ COPY requirements.txt /root/
 # Install the Python dependencies
 RUN pip install -r requirements.txt
 
+# Copy the pre-processing function
+COPY preprocessing.py /root/
+
 # Copy the application code to the container
 COPY app.py /root/
+
+#Download the models
+RUN mkdir -p /root/models
+RUN curl -L -o /root/models/c1_BoW_Sentiment_Model.pkl https://github.com/remla23-team2/model-training/raw/main/data/models/c1_BoW_Sentiment_Model.pkl
+RUN curl -L -o /root/models/c2_Classifier_Sentiment_Model https://github.com/remla23-team2/model-training/raw/main/data/models/c2_Classifier_Sentiment_Model
 
 # Set the entrypoint and default command for the container
 ENTRYPOINT ["python"]
--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import joblib
 from sklearn.feature_extraction.text import CountVectorizer
 import pickle
 from flask_cors import CORS
+import requests
+import io
 
 import re
 import nltk
@@ -11,12 +13,18 @@ nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 ps = PorterStemmer()
+from preprocessing import pre_processing
 
 app = Flask(__name__)
 swagger = Swagger(app)
 CORS(app)
 
 # Load the trained model
+cv = pickle.load(open('models/c1_BoW_Sentiment_Model.pkl', "rb"))
+
+# Load the Classifier Sentiment Model
+classifier = joblib.load('models/c2_Classifier_Sentiment_Model')
+ 
 
 @app.route('/', methods=['POST'])
 def predict():
@@ -42,9 +50,13 @@ def predict():
         description: Some result
     """
     msg = request.get_json().get('msg')
+    preprocessed_msg = pre_processing(msg)
+    prediction = classifier.predict([preprocessed_msg])
+    
     return {
-    "review": "this is a positive or negative review"
+    "review": "this is a positive or negative review" #talk to the group
 }
+   
 
 app.run(host="0.0.0.0", port=8080, debug=True)
 
