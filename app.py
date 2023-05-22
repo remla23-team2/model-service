5fa04194904fccaf8dac605bd71d7ea731c492db
Penzhi <tyypz2590477658@gmail.com>
2023-05-22 16:23:37 +0200

    try to work on histogram

--- app.py
+++ app.py
@@ -32,9 +32,10 @@ buffer_label = []
 
 predict_counter = Counter('predictions_counter', 'The total number of model predictions')
 model_accuracy = Gauge('model_accuracy', 'Accuracy of the predictions')
-input_data_size_distribution = Histogram(
+feedback_trend = Histogram(
     'feedback_trend', 'How the feedbacks change when time flies', 
-    buckets=[5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+    buckets=[5, 10, 15, 20, 25, 30, 35, 40, 45, 50],
+    labelnames=['bin']
 )
 
 def split_and_average(l, chunk_size):
@@ -49,7 +50,6 @@ def split_and_average(l, chunk_size):
 
 @app.route('/metrics', methods=['GET'])
 def metrics():
-
     num_correct = sum([int(x == y) for x, y in zip(buffer_predict, buffer_label)])
     accuracy = round(num_correct/len(buffer_label), 2)
     model_accuracy.set(accuracy)
@@ -57,6 +57,7 @@ def metrics():
     registry = prometheus_client.CollectorRegistry()
     registry.register(predict_counter)
     registry.register(model_accuracy)
+    registry.register(feedback_trend)
 
     return Response(prometheus_client.generate_latest(registry), mimetype="text/plain")
 
@@ -98,7 +99,15 @@ def predict():
     if len(buffer_predict) >= 50:
         buffer_predict.pop(0)
     buffer_predict.append(result)
-    averages = split_and_average(list(reversed(buffer_predict)), 5)
+
+    if len(buffer_predict) % 5 == 0: # we compute the average every 5 predictions
+        averages = split_and_average(list(reversed(buffer_predict)), 5)
+        if len(averages) >= 10:
+            averages.pop(0)
+        while len(averages) < 10: # fill in the remaining spots with 0's
+            averages.append(0)
+        for i, avg in enumerate(averages):
+            feedback_trend.labels(bin=i+1).observe(avg)
 
     # Attach the ground truth to another list to compute the success rate.
     label = input_data.get('ground_truth')
